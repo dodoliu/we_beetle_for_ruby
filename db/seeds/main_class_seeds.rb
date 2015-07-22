36c00a87e1633656f5cb7bfb96ce985f3973c3dc
LDY <liudongyue1987@163.com>
2015-07-22 23:47:32 +0800

    前端页面开发

--- db/seeds/main_class_seeds.rb
+++ db/seeds/main_class_seeds.rb
@@ -2,7 +2,7 @@
 
 #main_classes seed,算是正式数据了
 #tip: 执行这个seed前 需要将 main_class model中这个上传图片先屏蔽掉 mount_uploader :pic_url, ImageUploader
-MainClass.create(name: '公司简介', sid: SecureRandom.uuid, pic_url: 'company.jpg', status: 1, position: 1)
+MainClass.create(name: '公司简介', sid: SecureRandom.uuid, pic_url: 'company.jpg', status: 1, position: 1, url: 'http://mp.weixin.qq.com/s?__biz=MzAwMzExNDIzOA==&mid=201010916&idx=1&sn=3d3ca9af4dfe24163e1936c125260e01#rd')
 MainClass.create(name: '家装案例', sid: SecureRandom.uuid, pic_url: 'jiazhuang.jpg', status: 1, position: 1)
 MainClass.create(name: '工装案例', sid: SecureRandom.uuid, pic_url: 'gongzhuang.jpg', status: 1, position: 1)
 MainClass.create(name: '实景案例', sid: SecureRandom.uuid, pic_url: 'shijing.jpg', status: 1, position: 1)
